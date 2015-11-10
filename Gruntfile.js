3f9b2f1ed51bc48b9868ada8c54069bf91700f74
espretto <espretto@gmail.com>
2015-11-10 18:59:11 +0100

    added build tasks and instructions

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,69 @@
+
+var webpack = require('webpack')
+
 module.exports = function(grunt) {
 
   var pkg = grunt.file.readJSON('package.json')
 
   grunt.initConfig({
+
+    pkg: pkg,
+
     requirejs : {
-      funcr : {
+      extension : {
         options : {
           baseUrl: 'src/',
           name: 'funcr',
-          out: 'built/funcr.amd.js',
+          // optimize: 'none',
+          out: 'built/funcr.amd-<%=pkg.version%>.js',
         }
+      },
+      standalone: {
+        options : {
+          baseUrl: 'src/',
+          name: 'almond',
+          include: ['funcr'],
+          // optimize: 'none',
+          paths: {
+            almond: '../node_modules/almond/almond'
+          },
+          wrap: {
+            startFile: 'src/frag/almond.wrap.js',
+            endFile: 'src/frag/almond.up.js'
+          },
+          out: 'built/funcr.almond-<%=pkg.version%>.js'
+        }
+      }
+    },
+
+    webpack: {
+      standalone: {
+        context: './src',
+        entry: './funcr',
+        output: {
+          libraryTarget: "umd",
+          library: 'funcr',
+          path: 'built/',
+          filename: "funcr.webpack-<%=pkg.version%>.js"
+        },
+        plugins: [
+          new webpack.optimize.UglifyJsPlugin({
+            mangle: true,
+            compress: {
+              warnings: false,
+            }
+          })
+        ]
       }
     }
   })
 
   grunt.loadNpmTasks('grunt-requirejs')
+  grunt.loadNpmTasks("grunt-webpack");
 
-  grunt.registerTask('default', 'requirejs:funcr')
-};
+  grunt.registerTask('default', [
+    'requirejs:extension',
+    'requirejs:standalone',
+    'webpack:standalone'
+  ])
+}
--- README.md
+++ README.md
@@ -2,9 +2,42 @@ funcr
 =====
 utility library to help write functional javascript code.
 
-quick reference
----------------
+usage
+-----
+_funcr_ modules are written in _AMD_ syntax with an [amdefine][2]-header to support
+_CommonJS_ environments. assignments to the global `exports` object or per-module
+globals are not supported. however, the standalone builds do have an _UMD_-wrapper.
+```
+npm install
 
+# unwrapped rjs-build
+grunt requirejs:extension
+
+# umd-wrapped standalone with almond
+grunt requirejs:standalone
+
+# umd-wrapped standalone with webpack
+grunt webpack:standalone
+
+# convenience
+npm run gzip
+ls -la ./built
+```
+
+build | size
+----- | ----
+funcr.amd-0.7.0.js | 4.4kb
+funcr.amd-0.7.0.js.gz | 1.5kb
+funcr.almond-0.7.0.js | 7.6kb
+funcr.almond-0.7.0.js.gz | 2.9kb
+funcr.webpack-0.7.0.js | 5.3kb
+funcr.webpack-0.7.0.js.gz | 1.5kb
+
+__note:__ the _amdefine_-headers are stripped by each build using either anonymous
+build-pragmas (requirejs) or dead/unreachable code removal (webpack).
+
+reference
+---------------
 - `append(to, from)`
 - `array(length)`
 - `chsig(fn, ...idx)`
@@ -19,15 +52,7 @@ quick reference
 - `thisify(fn, ctx [,arity=fn.length])`
 - `uncurry(fn [,argc=fn.length])`
 
-build
------
-modules are written in CommonJS.
-```
-npm install -g webpack
-npm install
-npm run build
-npm run gzip
-```
+
 
 licence
 -------
